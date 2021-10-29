009d9dca2c2f2df2d3b4d6b2942cc5e04e514a45
leoflewis <fleonardolewis@gmail.com>
2021-10-29 11:12:33 -0500

    Order and OrderList

--- edu.ics372.gp1/edu/ics372/gp1/entities/Order.java
+++ edu.ics372.gp1/edu/ics372/gp1/entities/Order.java
@@ -1,22 +1,99 @@
 package edu.ics372.gp1.entities;
 
+/**
+ * Order object, an order consists of an order ID, product, 
+ * that product's ID and the quantity of the product.
+ * @author leo
+ *
+ */
 public class Order {
+	private String orderID;
+	private String productID;
 	private Product product;
 	private int quantity;
+	private static int idCounter = 0;
 
+	/**
+	 * Constructs order, creates unique ID.
+	 * @param productID
+	 * @param quantity
+	 * @param product
+	 */
+	public Order(Product product, int quantity) {
+		this.productID = product.getId();
+		this.quantity = quantity;
+		this.orderID = String.valueOf(idCounter);
+		this.product = product;
+		idCounter++;
+	}
+	
+	/**
+	 * @return Product in order.
+	 */
 	public Product getProduct() {
 		return product;
 	}
 
+	/**
+	 * Sets product for order.
+	 * @param product 
+	 */
 	public void setProduct(Product product) {
 		this.product = product;
 	}
 
+	/**
+	 * @return quantity of product.
+	 */
 	public int getQuantity() {
 		return quantity;
 	}
 
+	/**
+	 * Sets quantity in order.
+	 * @param quantity
+	 */
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+	
+	/**
+	 * @return Order ID.
+	 */
+	public String getOrderID() {
+		return orderID;
+	}
+	
+	/**
+	 * @return Product ID.
+	 */
+	public String getProductID(){
+		return productID;
+	}
+	
+	/**
+	 * Sets product ID.
+	 * @param productID
+	 */
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+	
+	/**
+	 * Sets order ID.
+	 * @param orderID
+	 */
+	public void setOrderID(String orderID){
+		this.orderID = orderID;
+	}
+	
+	/**
+	 * Constructs string for an order.
+	 */
+	public String toString(){
+		return "Order: " + orderID + ", product: " + productID + 
+				", " + product.getName() + ", quantity: " + quantity;
+	}
+	
+	
 }
