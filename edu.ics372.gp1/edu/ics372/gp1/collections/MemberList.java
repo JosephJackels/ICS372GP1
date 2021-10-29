a42044657eba1e8bd31b4536b5c7e1559b8c3f68
phana <phana@10.0.0.69>
2021-10-29 15:06:34 -0500

    update, added documentation

--- edu.ics372.gp1/edu/ics372/gp1/collections/MemberList.java
+++ edu.ics372.gp1/edu/ics372/gp1/collections/MemberList.java
@@ -6,14 +6,26 @@ import java.util.List;
 
 import edu.ics372.gp1.entities.Member;
 
+/**
+ * Memberlist class is use maintain a list of member
+ * @author Andy
+ *
+ */
 public class MemberList {
 	private List<Member> members = new LinkedList<Member>();
 	private static MemberList memberList;
 
+	/**
+	 * memberList constructor.
+	 */
 	private MemberList() {
 
 	}
 
+	/**
+	 * create an object if it hasnt created yet.
+	 * @return memberList
+	 */
 	public static MemberList getInstance() {
 		if (memberList == null) {
 			memberList = new MemberList();
@@ -21,10 +33,20 @@ public class MemberList {
 		return memberList;
 	}
 	
+	/**
+	 * adding member to the list
+	 * @param member
+	 * @return true or false.
+	 */
 	public boolean insertMember(Member member) {
 		return members.add(member);
 	}
 	
+	/**
+	 * get data and information of a member given their ID
+	 * @param memberId
+	 * @return member, null if there are no member found
+	 */
 	public Member getMember(String memberId) {
 		Iterator<Member> iterator = members.listIterator();
 		while(iterator.hasNext()) {
@@ -36,10 +58,18 @@ public class MemberList {
 		return null;
 	}
 	
+	/**
+	 * @return iterator of a memberList
+	 */
 	public Iterator<Member> getMembers(){
 		return members.listIterator();
 	}
 	
+	/**
+	 * remove member from the list given their ID
+	 * @param memberId
+	 * @return true if successful, false if not
+	 */
 	public boolean removeMember(String memberId) {
 		Iterator<Member> iterator = members.listIterator();
 		while(iterator.hasNext()) {
@@ -52,6 +82,11 @@ public class MemberList {
 		return false;
 	}
 	
+	/**
+	 * check a member's membership status given their Id
+	 * @param memberId
+	 * @return true if is a member, false if not.
+	 */
 	public boolean isMember(String memberId) {
 		Iterator<Member> iterator = members.listIterator();
 		while(iterator.hasNext()) {
