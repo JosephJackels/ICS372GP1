37d68caae4fc1619326d7b31d4dbb9d2d12ff45c
Dilli Khatiwoda <dilli.khatiwoda@yahoo.com>
2021-11-06 10:57:22 -0500

    Documented transaction class

--- edu.ics372.gp1/edu/ics372/gp1/entities/Transaction.java
+++ edu.ics372.gp1/edu/ics372/gp1/entities/Transaction.java
@@ -2,11 +2,24 @@ package edu.ics372.gp1.entities;
 
 import java.util.Calendar;
 
+/**
+ * This class represents the transaction object for each customer of the grocery
+ * store.
+ * 
+ * @author Joseph
+ *
+ */
 public class Transaction {
 	private String memberID;
 	private Calendar date;
 	private double totalPrice;
 
+	/**
+	 * Constructs transaction object
+	 * 
+	 * @param memberID
+	 * @param totalPrice
+	 */
 	public Transaction(String memberID, double totalPrice) {
 		this.memberID = memberID;
 		this.totalPrice = totalPrice;
@@ -25,6 +38,15 @@ public class Transaction {
 		return this.totalPrice;
 	}
 
+	/**
+	 * This method checks the transaction of a particular member in the specified
+	 * time period.
+	 * 
+	 * @param memberID  - ID of the member
+	 * @param startDate - start date for generating transaction
+	 * @param endDate   - end date for generating transaction for member
+	 * @return - true or false
+	 */
 	public boolean checkTransaction(String memberID, Calendar startDate, Calendar endDate) {
 
 		if (this.memberID != memberID) {
